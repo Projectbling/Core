c84ca52785f52b00abe338fdf5f58f6a940f61e8
ccooiinnddeexx <108065654+ccooiinnddeexx@users.noreply.github.com>
2024-02-27 00:20:32 +0300

    setup.py güncellendi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mediator",
+    name="projectBlinkMediator",
     version="0.1.0",
-    url="https://github.com/Projectbling/Core",
+    url="http://github.com/Projectbling/Core",
     author="ProjectBlink",
     author_email="author@gmail.com",
     description="Bir Mediator Pattern Kütüphanesi",
